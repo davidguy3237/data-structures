2675b48e908379a1b3f9d85cb5cd342d7796616a
davidguy3237 <davidtran3237@gmail.com>
2023-03-29 12:17:12 -0700

    Complete functional/queue & functional/stack

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,12 +7,23 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    var keys = Object.keys(storage);
+    if (keys.length > 0) {
+      storage[Number(keys[keys.length - 1]) + 1] = value;
+    } else {
+      storage['0'] = value;
+    }
   };
 
   someInstance.dequeue = function() {
+    var keys = Object.keys(storage);
+    var output = storage[keys[0]];
+    delete storage[keys[0]];
+    return output;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,23 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    var keys = Object.keys(storage);
+    if (keys.length > 0) {
+      storage[Number(keys[keys.length - 1]) + 1] = value;
+    } else {
+      storage['0'] = value;
+    }
   };
 
   someInstance.pop = function() {
+    var keys = Object.keys(storage);
+    var output = storage[keys[keys.length - 1]];
+    delete storage[keys[keys.length - 1]];
+    return output;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
