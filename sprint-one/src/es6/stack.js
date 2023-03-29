64ae4dffbecf79857f925c0d5ed20fe65a95e169
davidguy3237 <davidtran3237@gmail.com>
2023-03-29 15:52:08 -0700

    Complete es6/stack

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,30 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
   }
 
+  push(value) {
+    var keys = Object.keys(this.storage);
+    if (keys.length > 0) {
+      this.storage[keys[keys.length - 1] + 1] = value;
+    } else {
+      this.storage['0'] = value;
+    }
+  }
+
+  pop() {
+    var keys = Object.keys(this.storage);
+    if (keys.length > 0) {
+      var output = this.storage[keys[keys.length - 1]];
+      delete this.storage[keys[keys.length - 1]];
+      return output;
+    } else {
+      return;
+    }
+  }
+
+  size() {
+    return Object.keys(this.storage).length;
+  }
 }
